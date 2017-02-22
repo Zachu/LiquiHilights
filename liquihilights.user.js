b809ead9c680280c323efc8b463aef411cbbafa0
Jani Korhonen <zachu@thegroup.fi>
2017-02-22 18:16:35 +0200

    Clean up the code

--- liquihilights.user.js
+++ liquihilights.user.js
@@ -2,7 +2,7 @@
 // @name         LiquiHilights
 // @namespace    http://zachu.fi/
 // @downloadURL  https://github.com/Zachu/LiquiHilights/raw/master/liquihilights.user.js
-// @version      0.3.2
+// @version      0.3.3
 // @description  Adds upcoming matches of selected teams to the header of Liquipedia main page
 // @author       Jani Korhonen <zachu@thegroup.fi>
 // @match        http://wiki.teamliquid.net/*/Main_Page
@@ -12,69 +12,89 @@
 // @require      https://code.jquery.com/jquery-3.1.1.slim.min.js
 // ==/UserScript==
 
-var supportedWikis = ['dota2', 'overwatch', 'rocketleague'],
-    wiki = window.location.href.split('/')[3],
-    teams_key = 'teams.' + wiki,
-    teams = GM_getValue(teams_key);
-
-if (!(teams instanceof Array)) {
-    teams = [];
-}
-
 (function() {
     'use strict';
 
-    if ($.inArray(wiki, supportedWikis) === -1) {
+    // Initialize
+    var supportedWikis = ['dota2', 'overwatch', 'rocketleague'],
+        wiki = window.location.href.split('/')[3],
+        teams_key = 'teams.' + wiki,
+        teams = GM_getValue(teams_key),
+        hilight_matches = [];
+
+    if (!(teams instanceof Array)) {
+        teams = [];
+    }
+
+    if (supportedWikis.indexOf(wiki) === -1) {
         console.log("Liquilights: " + wiki + " not supported. Supported wikis are " + supportedWikis.join(', '));
         return;
     }
 
-    // Find teams in page
-    var hilight_matches = [];
-    $(".main-page-banner ~ .row .team-left, .main-page-banner ~ .row .team-right").each(function(i, e) {
-        var table = $(e).closest("table"),
-            href = $(e).find('a:not([href="#"])').attr('href'),
-            delFavLink = $('<a href="#" class="delFav" title="Remove team from hilights"><span class="fa fa-fw fa-star"></span></a>'),
-            addFavLink = $('<a href="#" class="addFav" title="Add team to hilights"><span class="fa fa-fw fa-star-o"></span></a>');
+    // Define functions
+    function is_favorited(teamHref) {
+        return teams.indexOf(teamHref) !== -1;
+    }
 
-        // Add favorite buttons
-        if ($.inArray(href, teams) !== -1) {
-            if ($(e).find('a').length === 0) {
-                return;
-            } else if ($(e).hasClass('team-left')) {
-                $(e).prepend(delFavLink.css('margin-right', '1em'));
-            } else {
-                $(e).append(delFavLink.css('margin-left', '1em'));
-            }
+    function is_hilighted(matchTable) {
+        return hilight_matches.indexOf(matchTable) !== -1;
+    }
+
+    function add_links(teamTd, teamHref) {
+        var add = '<a href="#" class="addFav" title="Add team to hilights"><span class="fa fa-fw fa-star-o"></span></a>',
+            del = '<a href="#" class="delFav" title="Remove team from hilights"><span class="fa fa-fw fa-star"></span></a>',
+            link;
+
+        if (is_favorited(teamHref)) {
+            link = del;
         } else {
-            if ($(e).find('a').length === 0) {
-                return;
-            } else if ($(e).hasClass('team-left')) {
-                $(e).prepend(addFavLink.css('margin-right', '1em'));
-            } else {
-                $(e).append(addFavLink.css('margin-left', '1em'));
-            }
-
+            link = add;
         }
-        // Team is in hilight list
-        if ($.inArray(href, teams) !== -1 && $.inArray(table, hilight_matches) === -1) {
-            hilight_matches.push(table.clone());
+
+        if ($(teamTd).hasClass('team-left')) {
+            $(teamTd).prepend($(link).css('margin-right', '1em'));
+        } else {
+            $(teamTd).append($(link).css('margin-left', '1em'));
+        }
+    }
+
+    // Find teams in page. This is the easiest way I could figure to get all match rowstables
+    $(".main-page-banner ~ .row .team-left, .main-page-banner ~ .row .team-right").each(function(i, teamTd) {
+        var matchTable = $(teamTd).closest("table"),
+            teamHref = $(teamTd).find('a:not([href="#"])').attr('href');
+
+        if (typeof teamHref === 'undefined') {
+            // Team is some sort of TBA or something. Skip
+            return;
+        }
+
+        // Add stars/unstars
+        add_links(teamTd, teamHref);
+
+        // Match should be hilighted
+        if (is_favorited(teamHref) && !is_hilighted(matchTable)) {
+            hilight_matches.push(matchTable);
         }
     });
 
     // Add matches to header
+    var matchesEl = $('<div class="liquihilights">');
+    hilight_matches.forEach(function(matchTable) {
+        matchesEl.append(matchTable.clone());
+    });
+
     $(".main-page-banner").children().wrapAll('<div class="col-md-6 col-md-pull-6">');
     $(".main-page-banner").addClass('row').prepend(
         $("<div>").addClass("col-md-6 col-md-push-6").append(
             $("<h2>Upcoming hilights</h2>").css("margin-top", 0).css("padding-top", 0)
-        ).append(hilight_matches)
+        ).append(matchesEl)
     );
 
     $('.addFav').on('click', function(e) {
         e.preventDefault();
         var href = $(this).closest('td').find('a:not([href="#"])').attr('href');
 
-        if ($.inArray(href, teams) === -1) {
+        if (!is_favorited(href)) {
             teams.push(href);
             GM_setValue(teams_key, teams);
             $(this).find('span.fa-star-o').removeClass('fa-star-o').addClass('fa-star');
@@ -85,33 +105,11 @@ if (!(teams instanceof Array)) {
     $('.delFav').on('click', function(e) {
         e.preventDefault();
         var href = $(this).closest('td').find('a:not([href="#"])').attr('href');
-        if ($.inArray(href, teams) !== -1) {
-            teams.splice($.inArray(href, teams), 1);
+        if (is_favorited(href)) {
+            teams.splice(teams.indexOf(href), 1);
             GM_setValue(teams_key, teams);
             $(this).find('span.fa-star').removeClass('fa-star').addClass('fa-star-o');
         }
         return false;
     });
 })();
-
-function add_fav_link(e) {
-    var link = '<a href="#" class="addFav" title="Add team to hilights"><span class="fa fa-fw fa-star-o"></span></a>';
-    if ($(e).find('a').length === 0) {
-        return;
-    } else if ($(e).hasClass('team-left')) {
-        $(e).prepend($(link).css('margin-right', '1em'));
-    } else {
-        $(e).append($(link).css('margin-left', '1em'));
-    }
-}
-
-function del_fav_link(e) {
-    var link = '<a href="#" class="delFav" title="Remove team from hilights"><span class="fa fa-fw fa-star"></span></a>';
-    if ($(e).find('a').length === 0) {
-        return;
-    } else if ($(e).hasClass('team-left')) {
-        $(e).prepend($(link).css('margin-right', '1em'));
-    } else {
-        $(e).append($(link).css('margin-left', '1em'));
-    }
-}
